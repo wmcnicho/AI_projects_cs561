3c8f5401d7d5219aa9542eaf9b50db729190486e
William McNichols <wmcnicho@usc.edu>
2015-09-23 13:28:10 -0700

    Finished BFS implementation. Using c++11 constructs and have yet to test on vocarem.

--- hw1/waterFlow.cpp
+++ hw1/waterFlow.cpp
@@ -4,174 +4,282 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <queue>
+#include <set>
+#include <algorithm>
 
 class Edge;
 struct Node;
 class Graph;
 enum Node_type {SOURCE, DEST, MID};
 
-void run_BFS(Graph &g, int start_time);
+void run_BFS(Graph &g, int start_time, std::ofstream &output);
 void run_DFS(Graph &g, int start_time);
 void run_UCS(Graph &g, int start_time);
 
 
 class Graph{
+    Node *source;
+    std::vector<Node*> nodes;
+    std::vector<Edge*> edges;
 public:
-	Node *source;
-	Graph();
-	~Graph();
-
-	void create_node(const char *name, Node_type type);
-	void add_edge(const char *start_name, char *end_name, int length);
-
-	std::vector<Node> nodes;
+    ~Graph();
+    void create_node(std::string name, Node_type type);
+    void add_edge(std::string start_name, std::string end_name, int length);
+    Node *getSource();
+    Graph();
+    
 };
-
-Graph::Graph(){
-
-}
-
-Graph::~Graph(){
-	//going to need to write this to clean up memory
-}
-
-void Graph::create_node(const char *name, Node_type type){
-
-}
-
-void Graph::add_edge(const char *start_name, char *end_name, int length){
-	
-}
-
-
-int main(int argc, char *argv[]){
-	if(argc < 2){
-		std::cout << "Fail, not enough args" << std::endl;
-		return 1;
-	}
-	
-	std::ifstream input_file(argv[2]);
-	if(!input_file.is_open()){
-		std::cout << "File not found" << std::endl;
-		return 1;
-	}
-
-	while(!input_file.eof()){
-
-		//parse a single simulation worth of data
-		// <task> algorithm that you are supposed to use for this case
-		std::string alg;
-		input_file >> alg;
-
-		Graph *g = new Graph();
-		// <source> name of the source node
-		char *source_name;
-		input_file >> source_name;
-		g->create_node(source_name, SOURCE);
-		// <destinations> names of the destination nodes
-		std::string name;
-		while(std::getline(input_file, name, ' ')){
-			g->create_node(name.c_str(), DEST);
-		}
-
-		// <middle nodes> names of the middle nodes
-		while(std::getline(input_file, name, ' ')){
-			g->create_node(name.c_str(), MID);
-		}
-
-		// <#pipes> represents the number of pipes
-		int num_pipes;
-		input_file >> num_pipes;
-
-		// <graph> represents start-end nodes, lengths and off-times of pipes
-		for (int i = 0; i < num_pipes; ++i)
-		{
-			//<start> <end> <length> <#off-periods> < period1 > .... <periodn>
-			char *start;
-			char *end;
-			int length;
-			int num_periods;
-			input_file >> start >> end >> length >> num_periods;
-
-			g->add_edge(start, end, length);
-
-			//ignoring off-periods for now because not starting UCS yet
-			std::string buff;
-			std::getline(input_file, buff);
-		}
-
-		int start_time;
-		input_file >> start_time;
-
-		//run simulation
-		if(alg == "BFS"){
-			run_BFS(*g, start_time);
-		}
-		else{
-			std::cout << "Haven't written these yet" << std::endl;
-		}
-
-		//write to file and clean up memory
-
-	}
-
-std::cout << "Hello World" << std::endl;
-return 0;
-}
-
-
-void run_BFS(Graph &g, int start_time){
-
-}
-
-
-
 
 //Graph data structure, this may change significantly
 class Edge
 {
-	Edge(Node* source, Node* dest, int length, int *active_hours);
-	~Edge();
-	bool checkActive(int time);
-
-	Node* source;
-	Node* dest;
-	int length;
-	int *active_hours;
-
 public:
+    Node* source;
+    Node* dest;
+    int length;
+    int *active_hours;
+    
+    
+    Edge(Node* source, Node* dest, int length, int *active_hours);
+    bool checkActive(int time);
 };
 
-Edge::Edge(Node* source, Node* dest, int length, int *active_hours):
- source(source), dest(dest), length(length), active_hours(active_hours) 
-{
-
-}
-
-Edge::~Edge(){
-	//?
-}
-
-bool Edge::checkActive(int time){
-	std::cout << "Write this" << std::endl;
-	return false;
-}
 
 class Node
 {
-	Node(Node_type n, char* name);
-
-	std::vector<Edge*> edges;
-	Node_type type;
-	char* name;
-
+    std::vector<Edge*> edges;
+    std::vector<Node*> children;
+    
 public:
-	void add_edge(Edge* e);
+    Node(Node_type n, std::string name);
+    Node_type type;
+    std::string name;
+    void add_edge(Edge* e);
+    std::vector<Node*> &getChildren();
+    int getNumChildren();
 };
 
-Node::Node(Node_type n, char* name): type(n), name(name){
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        std::cout << "Fail, not enough args" << std::endl;
+        return 1;
+    }
+    
+    std::ifstream input_file(argv[2]);
+    if(!input_file.is_open()){
+        std::cout << "File not found" << std::endl;
+        return 1;
+    }
+    
+    int num_sims;
+    input_file >> num_sims;
+    
+    std::ofstream output("output.txt");
+    
+    while(!input_file.eof()){
+        
+        //parse a single simulation worth of data
+        // <task> algorithm that you are supposed to use for this case
+        std::string alg;
+        input_file >> alg;
+        
+        Graph *g = new Graph();
+        // <source> name of the source node
+        std::string source_name;
+        input_file >> source_name;
+        input_file.ignore();
+        input_file.ignore();
+        g->create_node(source_name, SOURCE);
+        // <destinations> names of the destination nodes
+        std::string line;
+        if(std::getline(input_file, line)){
+            std::istringstream iss(line);
+            std::string name;
+            while(iss >> name){
+                g->create_node(name, DEST);
+            }
+        }
+        
+        // <middle nodes> names of the middle nodes
+        if(std::getline(input_file, line)){
+            std::istringstream iss(line);
+            std::string name;
+            while(iss >> name){
+                g->create_node(name, MID);
+            }
+        }
+        
+        
+        // <#pipes> represents the number of pipes
+        int num_pipes;
+        input_file >> num_pipes;
+        
+        // <graph> represents start-end nodes, lengths and off-times of pipes
+        for (int i = 0; i < num_pipes; ++i)
+        {
+            //<start> <end> <length> <#off-periods> < period1 > .... <periodn>
+            std::string start;
+            std::string end;
+            int length;
+            int num_periods;
+            input_file >> start >> end >> length >> num_periods;
+            
+            g->add_edge(start, end, length);
+            
+            //ignoring off-periods for now because not starting UCS yet
+            std::string buff;
+            std::getline(input_file, buff);
+        }
+        
+        int start_time;
+        input_file >> start_time;
+        
+        //run simulation
+        if(alg == "BFS"){
+            run_BFS(*g, start_time, output);
+        }
+        else{
+            std::cout << "Haven't written these yet" << std::endl;
+        }
+        delete g;
+    }
+    
+    std::cout << "Finished" << std::endl;
+    return 0;
+}
+
+
+void run_BFS(Graph &g, int start_time, std::ofstream& output){
+    int current_time = start_time;
+    Node *n = g.getSource();
+    if(n->type == DEST){
+        std::cout << n->name << " " << current_time << std::endl;
+        output << n->name << " " << current_time << std::endl;
+        return;
+    }
+    std::queue<Node*> frontier;
+    frontier.push(n);
+    
+    std::set<Node*> explored;
+    
+    while(!frontier.empty()){
+        n = frontier.front();
+        frontier.pop();
+        explored.insert(n);
+        if(n->type == DEST){
+            //success
+            std::cout << n->name << " " << current_time << std::endl;
+            output << n->name << " " << current_time << std::endl;
+            return;
+        }
+        //queue children into frontier
+        std::vector<Node*> children = n->getChildren();
+        for (int i=0; i<children.size(); ++i) {
+            if(explored.find(children[i]) == explored.end()){//If the node has not already been explored
+                frontier.push(children[i]);
+            }
+        }
+        //increment time
+        current_time++;
+
+    }
+    //if this while loop finishes then all nodes were checked and no suceess was found
+    std::cout << "None" << std::endl;
+    output << "None" << std::endl;
+    return;
+    
+    
+}
+
+
+//Graph implementation
+Graph::Graph(){
+    
+}
+
+Graph::~Graph(){
+    //clean up nodes and edges created in graph
+    for(auto it = nodes.begin(); it != nodes.end(); ++it) {
+        delete *it;
+    }
+    for(auto it = edges.begin(); it != edges.end(); ++it) {
+        delete *it;
+    }
+
+}
+
+void Graph::create_node(std::string name, Node_type type){
+    Node *new_node = new Node(type, name);
+    if(type == SOURCE){
+        source = new_node;
+    }
+    //Does ordering matter?
+    nodes.push_back(new_node);
+}
+
+void Graph::add_edge(std::string start_name, std::string end_name, int length){
+    Node *start = nullptr;
+    Node *end = nullptr;
+    for (int i = 0; i < nodes.size(); ++i)
+    {
+        if(nodes[i]->name == start_name){
+            start = nodes[i];
+        }
+        if(nodes[i]->name == end_name){
+            end = nodes[i];
+        }
+    }
+    if(!start || !end){
+        std::cout << "Problem in adding edge" << std::endl;
+        return;
+    }
+    Edge *e = new Edge(start, end, length, nullptr);
+    edges.push_back(e);
+    start->add_edge(e);
+}
+
+
+
+//Edge Implementation
+Edge::Edge(Node* source, Node* dest, int length, int *active_hours):
+source(source), dest(dest), length(length), active_hours(active_hours) 
+{
+    
+}
+
+
+bool Edge::checkActive(int time){
+    std::cout << "Write this" << std::endl;
+    return false;
+}
+
+
+//Node implementation
+Node::Node(Node_type n, std::string name): type(n), name(name){
+}
+
+
+Node* Graph::getSource(){
+    return source;
+}
+
+std::vector<Node*> &Node::getChildren(){
+    //This might be unnessary you could probably just return and then in the add_edge deal with sorting
+    children.clear();
+    for (int i=0; i<edges.size(); ++i) {
+        children.push_back(edges[i]->dest);
+    }
+    //TODO: sort children alphabetically before returning
+    return children;
+}
+
+int Node::getNumChildren(){
+    return static_cast<int>(edges.size());
 }
 
 void Node::add_edge(Edge* e){
-	//edges.push_back(e);
+    edges.push_back(e);
 }
